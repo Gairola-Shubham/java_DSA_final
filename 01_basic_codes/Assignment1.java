32a8b4bfcddbbd69967acf40188b6502919cd2b0
Shubham Gairola <shubhamgairola124@gmail.com>
2025-07-01 12:08:56 +0530

    Create Assignment1.java

---
+++ 01_basic_codes/Assignment1.java
@@ -0,0 +1,20 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("*");
+    }
+    
+}
